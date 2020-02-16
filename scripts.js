c1c54d17e563f957a0bb6122bc982b306e8b76a5
Pedro Lana <pedroalana@hotmail.com>
2020-02-15 23:46:51 -0300

    Finalizando modal, sobre e receitas html

--- scripts.js
+++ scripts.js
@@ -3,7 +3,15 @@ const receitas = document.querySelectorAll('.receita');
 
 for (let receita of receitas) {
     receita.addEventListener("click",function(){
-        modaloverlay.classList.add('active')
+    const imageReceita = receita.querySelector("img").src;
+    const tituloReceita = receita.getElementsByTagName("p")[0].innerHTML;
+    const autorReceita = receita.getElementsByTagName("h3")[0].innerHTML;
+
+        modaloverlay.classList.add('active');
+        modaloverlay.querySelector('img').src=`${imageReceita}`;
+        modaloverlay.getElementsByTagName("p")[0].innerHTML = `${tituloReceita}`;
+        modaloverlay.getElementsByTagName("h3")[0].innerHTML = `${autorReceita}`;
+        
     })
 }
 
